9771793499a0c70c8a7dafc8f234d17e63695cef
mojeed-painless <shittumjd00@gmail.com>
2025-11-19 11:06:04 +0100

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+
+      <Link to='/login'>Login</Link>
     </div>
   );
 }
